714746392ac77dedb610f37d134001884a468efe
Gabriel A. Munck P. da Cruz <munckgabriel@gmail.com>
2023-10-24 14:04:47 -0300

    :sparkles: O inicio de um projeto sobre mim

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bambam">
+      <section className='bimbim'>
+        <div className='card'>
+          <img src={require('./assets/avatar.png')} alt="Avatar" />
+          <p className='name'>Gabriel Munck</p>
+          <label className='func'> {'< Web-Developer Junior >'} </label>
+          <p className='intro'>Welcome to my page!!!Let me show you a little bit of myself... </p>
+          <button className='skills'> Skills </button>
+        </div>
+      </section>
+
+
+
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Montserrat+Alternates:wght@100;300;500;700&display=swap" rel="stylesheet"/>
     </div>
+
+    
   );
 }
 
